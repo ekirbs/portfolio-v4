91eecc03d1c3013c59b45a1f193ff3f11f9609f2
Eric K <erickirberger@gmail.com>
2023-03-29 12:50:52 -0400

    adding google map

---
+++ src/pages/Map/index.js
@@ -0,0 +1,12 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export default function Map() {
+  return (
+    <>
+      <div>
+        Map
+      </div>
+    </>
+  )
+};
